a1e75875b029d66d24bc5b156b259af560ff9c21
khushboo204 <khushboorathod20@gmail.com>
2024-02-04 15:46:31 -0500

    Menu created to play with pet.

--- Petselection/Petselection/Program.cs
+++ Petselection/Petselection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 class Program
 {
@@ -11,7 +12,9 @@ class Program
         Console.WriteLine("3. Rabbit");
 
         string petChoice = Console.ReadLine();
+        Console.WriteLine("User Input: " + petChoice);
 
+        //Check the type entered by User and show the response.
         if (petChoice == "1")
         {
             Console.WriteLine("You have selected a Cat as your pet.");
@@ -26,10 +29,61 @@ class Program
         }
         else {
             Console.WriteLine("Invalid choice");
+            return;
         }
+        Console.WriteLine("What would you like to name your pet?");
+        
+        string petName = Console.ReadLine();
+        Console.WriteLine("User Input: " + petName);
 
-        
-        
-        
+        Console.WriteLine("Welcome, "+ petName +"! Let's take good care of him.");
+
+        int hunger = 0;
+        int happiness = 0;
+        int health = 0;
+
+        //keep allowing user to select from menu until pressed exit.
+        string menuSelection;
+        do
+        {
+            //Give user choice to select from menu
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Feed " + petName);
+            Console.WriteLine("2. Play with " + petName);
+            Console.WriteLine("3. Let " + petName + " rest");
+            Console.WriteLine("4. Check " + petName + "'s status");
+            Console.WriteLine("5. Exit");
+
+            menuSelection = Console.ReadLine();
+
+            //On base of selection show the health, happiness and hunger.
+            switch (menuSelection)
+            {
+                case "1":
+                    hunger -= 1;
+                    if (hunger < 0) hunger = 0;
+
+                    health += 1;
+                    if (health > 10) health = 10;
+
+                    Console.WriteLine("Hunger decreased, health increased.");
+                    break;
+                case "2":
+                    
+                    break;
+                case "3":
+                    
+                    break;
+                case "4":
+                    
+                    break;
+                case "5":
+                    Console.WriteLine("Thank you for playing with " + petName + ". Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid menu selection.");
+                    break;
+            }
+        } while (menuSelection != "5");
     }
 }
